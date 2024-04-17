61c157251c0bea1e4d4ad8627a9f4973c472f3d6
haneul02 <redlight02@naver.com>
2024-04-17 12:30:45 +0900

    카메라 팅기는 오류 수정

--- teemoV1/src/recognition/Userrecognition.js
+++ teemoV1/src/recognition/Userrecognition.js
@@ -1,45 +1,14 @@
-import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 
-class Userrecognition extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <RNCamera
-          style={styles.preview}
-          type={RNCamera.Constants.Type.front}
-          faceDetectionLandmarks={RNCamera.Constants.FaceDetection.Landmarks.all}
-          onFacesDetected={this.onFacesDetected}
-          onFaceDetectionError={this.onFaceDetectionError}
-          faceDetectionMode={RNCamera.Constants.FaceDetection.Mode.fast}
-        />
-      </View>
-    );
-  }
-
-  onFacesDetected = ({ faces }) => {
-    if (faces.length > 0) {
-      console.log('Faces detected!', faces);
-    }
-  };
-
-  onFaceDetectionError = (error) => {
-    console.error('Face Detection Error:', error);
-  };
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black',
-  },
-  preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-});
+const Userrecognition = () => (
+  <View style={{ flex: 1 }}>
+    <RNCamera
+      style={{ flex: 1 }}
+      type={RNCamera.Constants.Type.front}
+    />
+  </View>
+);
 
 export default Userrecognition;
