5923265e8d865b8b5b07731c6772c761d3bf2075
suji_2 <tnwl31091@gmail.com>
2024-04-27 20:20:35 +0900

    완성 x

--- teemoV1/src/screens/Record/RecordScreen.js
+++ teemoV1/src/screens/Record/RecordScreen.js
@@ -1,33 +1,61 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
-import { RNCamera } from 'react-native-camera';
-import Video from 'react-native-video';
+import { Camera, useCameraDevice } from 'react-native-vision-camera';
 
-// 영상 녹화 화면 코드
+//영상 녹화 화면 코드
 const RecordScreen = () => {
+  const [cameraPermission, setCameraPermission] = useState('not-determined');
+  const [devices, setDevices] = useState([]);
+  const [selectedDevice, setSelectedDevice] = useState(null);
   const [recording, setRecording] = useState(false);
   const [videoPath, setVideoPath] = useState(null);
   const [processing, setProcessing] = useState(false);
+  const device = useCameraDevice('front')
+
   const cameraRef = useRef(null);
-  const playerRef = useRef(null);
 
-  const startRecording = async () => { //녹화 시작
-    setRecording(true);
-    const options = { quality: RNCamera.Constants.VideoQuality["720p"] };
-    const { uri, codec = 'mp4' } = await cameraRef.current.recordAsync(options);
-    if (uri) {
-      console.log(uri);
-      setRecording(false);
-      setVideoPath(uri);
-      setProcessing(true);
+  useEffect(() => {
+    const checkPermissionAndLoadDevices = async () => {
+      let permission = await Camera.getCameraPermissionStatus();
+      if (permission === 'not-determined') {
+        permission = await Camera.requestCameraPermission();
+      }
+      setCameraPermission(permission);
+  
+      if (permission === 'authorized') {
+        const availableDevices = await Camera.getAvailableCameraDevices();
+        setDevices(availableDevices);
+        const frontCamera = availableDevices.find(device => device.position === 'front');
+        setSelectedDevice(frontCamera); // 전면 카메라로 변경
+      } else {
+        alert('카메라 권한이 필요합니다. 권한을 허용해주세요.');
+      }
+    };
+  
+    checkPermissionAndLoadDevices();
+  }, []);  
+
+  const startRecording = async () => {
+    if (cameraRef.current) {
+      setRecording(true);
+      try {
+        const video = await cameraRef.current.recordAsync();
+        setRecording(false);
+        setVideoPath(video.uri);
+        setProcessing(true);
+      } catch (error) {
+        console.error('녹화 오류 :', error);
+      }
+    } else {
+      console.error('카메라가 준비 되지 않았습니다.');
     }
-  };
+  };  
 
-  const stopRecording = () => { //녹화 중지
+  const stopRecording = () => {
     cameraRef.current.stopRecording();
   };
 
-  const resumeVideo = () => { //영상 확인
+  const resumeVideo = () => {
     setVideoPath(null);
     setProcessing(false);
   };
@@ -36,7 +64,7 @@ const RecordScreen = () => {
     <TouchableOpacity
       onPress={startRecording}
       style={styles.button}>
-      <Text style={{ fontSize: 14 }}> 녹화시작 </Text>
+      <Text style={{ fontSize: 14 }}> 녹화 시작 </Text>
     </TouchableOpacity>
   );
 
@@ -52,48 +80,27 @@ const RecordScreen = () => {
 
   return (
     <View style={styles.container}>
-      {!processing ?
-        <RNCamera
+      {!processing && selectedDevice && (
+        <Camera
           ref={cameraRef}
-          style={styles.preview}
-          type={RNCamera.Constants.Type.back}
-          flashMode={RNCamera.Constants.FlashMode.off}
-          androidCameraPermissionOptions={{
-            title: '카메라 사용 권한',
-            message: '귀하의 카메라를 사용하려면 권한이 필요합니다',
-            buttonPositive: '확인',
-            buttonNegative: '취소',
-          }}
-          androidRecordAudioPermissionOptions={{
-            title: '오디오 녹음 사용 권한',
-            message: '귀하의 오디오를 사용하려면 권한이 필요합니다',
-            buttonPositive: '확인',
-            buttonNegative: '취소',
-          }}
-          android
+          style={styles.camera}
+          cameraFlipType={'front'} // 전면 카메라
+          autoFocus={'on'}
+          isActive={true}
+          device={device}
         />
-        : null
-      }
-      <View
-        style={{ flex: 0, flexDirection: "row", justifyContent: "center" }}>
+      )}
+      <View style={{ flex: 0, flexDirection: "row", justifyContent: "center" }}>
         {button}
       </View>
 
-      {videoPath ? 
-        <View
-          style={{ flex: 1, flexDirection: "column", justifyContent: "center" }}>
-          <Video source={{ uri: videoPath }} 
-            ref={playerRef}
-             /* onBuffer={() => { }}                // Callback when remote video is buffering
-            onError={() => { }}               // Callback when video cannot be loaded */
-            style={styles.backgroundVideo} />
-          <TouchableOpacity style={styles.button}
-            onPress={resumeVideo}>
+      {videoPath && (
+        <View style={{ flex: 1, flexDirection: "column", justifyContent: "center" }}>
+          <TouchableOpacity style={styles.button} onPress={resumeVideo}>
             <Text>확인</Text>
           </TouchableOpacity>
         </View>
-        : null
-      }
+      )}
     </View>
   );
 };
@@ -104,7 +111,7 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     backgroundColor: 'black',
   },
-  preview: {
+  camera: {
     flex: 1,
     justifyContent: 'flex-end',
     alignItems: 'center',
@@ -117,13 +124,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     alignSelf: 'center',
     margin: 20,
-  },
-  backgroundVideo: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
   },
 });
 
